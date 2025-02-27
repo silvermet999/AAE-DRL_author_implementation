1136da8999c8caa95f987dcb5f20f3aad26293b9
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2025-02-27 10:45:30 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
-import pickle
+import argparse
+import sys
 
 import pandas as pd
-from scipy.stats import gamma, rayleigh, expon, chi2, exponpow, lognorm, norm
 from torch.utils.data import Dataset, DataLoader, Subset
 
 import numpy as np
@@ -10,29 +10,16 @@ from data import main_u
 
 cuda = True if torch.cuda.is_available() else False
 
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--X_ds', default="/home/silver/PycharmProjects/AAEDRL/DRL/rl_bal1.csv")
+    parser.add_argument('--y_ds', default="/home/silver/PycharmProjects/AAEDRL/clfs/labels.csv")
 
-def custom_dist(size):
-    for i, _ in enumerate(main_u.X.columns):
-        if i in [22]:
-            z = lognorm(1, 0, 0).rvs(size)
-        elif i in [0, 27, 28, 29]:
-            z = chi2(0.8, 0, 0.4).rvs(size)
-        elif i in [1, 6, 7, 8, 9, 11, 12, 16, 23]:
-            z = rayleigh(0, 0).rvs(size)
-        elif i in [17, 18, 24, 26]:
-            z= gamma(0.1, 0, 0.2).rvs(size)
-        elif i in [3]:
-            z=norm(1.4, 0.8).rvs(size)
-        elif i in [2, 25]:
-            z = exponpow(1.5, 0.1, 1).rvs(size)
-        else:
-            z = expon(0, 0).rvs(size)
+    return parser.parse_args(args)
 
-        z = torch.tensor(z, dtype=torch.float32).cuda() if cuda else torch.tensor(z, dtype=torch.float32)
-    return z
-
+# discrete features
 discrete = {
-    # "attack_cat": 10,
             "state": 5,
             # "service": 13,
             "ct_state_ttl": 6,
@@ -40,12 +27,13 @@ discrete = {
             # "sttl": 13,
             "trans_depth": 11
 }
-
+# binary features
 binary = ["proto", "is_ftp_login"]
+# the rest are continuous features
 discrete_and_binary = set(discrete.keys()).union(set(binary))
 continuous = [feature for feature in main_u.X_train.columns if feature not in discrete_and_binary]
 
-
+# When running the decoder
 def types_append(decoder, discrete_out, continuous_out, binary_out, discrete_samples, continuous_samples, binary_samples):
     for feature in decoder.discrete_features:
         discrete_samples[feature].append(torch.argmax(torch.round(discrete_out[feature]), dim=-1))
@@ -69,8 +57,6 @@ def type_concat(decoder, discrete_samples, continuous_samples, binary_samples):
 
     return discrete_samples, continuous_samples, binary_samples
 
-
-
 def all_samples(discrete_samples, continuous_samples, binary_samples):
     discrete_tensors = list(discrete_samples.values())
     continuous_tensors = list(continuous_samples.values())
@@ -81,7 +67,7 @@ def all_samples(discrete_samples, continuous_samples, binary_samples):
     combined = torch.cat(all_tensors, dim=1)
     return combined
 
-
+# Dataframe to Pytorch dataset
 class CustomDataset(Dataset):
     def __init__(self, data, labels):
         self.data = data
@@ -95,7 +81,7 @@ class CustomDataset(Dataset):
         label = torch.tensor(self.labels[idx], dtype=torch.long)
         return sample, label
 
-
+# Train eval test split of pytorch dataset
 def dataset_function(dataset, batch_size_t, batch_size_o, train=True):
     total_size = len(dataset)
     test_size = total_size // 5
@@ -114,13 +100,12 @@ def dataset_function(dataset, batch_size_t, batch_size_o, train=True):
 
         return test_loader
 
-
+# inverse scaling (in benchmark classification)
 def inverse_sc_cont(X, synth):
     synth_inv = synth * (X.max() - X.min()) + X.min()
     return pd.DataFrame(synth_inv, columns=X.columns, index=synth.index)
 
-
-
+# choose unaugmented or augmented dataset
 def dataset(original=False, train=True):
     if original:
         if train:
@@ -128,10 +113,11 @@ def dataset(original=False, train=True):
         else:
             dataset = CustomDataset(main_u.X_test_sc.to_numpy(), main_u.y_test.to_numpy())
     else:
+        args = parse_args(sys.argv[1:])
         df_org = pd.concat([main_u.X_sc, main_u.y], axis=1)
-        X_rl = pd.DataFrame(pd.read_csv("/home/silver/PycharmProjects/AAEDRL/DDPG/rl_bal1.csv"))
+        X_rl = pd.DataFrame(pd.read_csv(f"{args.X_ds}"))
         X_rl = X_rl.apply(lambda col: col.str.strip("[]").astype(float) if col.dtype == "object" else col)
-        y_rl = pd.DataFrame(pd.read_csv("/home/silver/PycharmProjects/AAEDRL/clfs/labels.csv"))
+        y_rl = pd.DataFrame(pd.read_csv(f"{args.y_ds}"))
         df_rl = pd.concat([X_rl, y_rl], axis=1)
         df_rl = df_rl[df_rl["attack_cat"] != 2]
         df = pd.concat([df_org, df_rl], axis=0)
@@ -145,8 +131,7 @@ def dataset(original=False, train=True):
             dataset = CustomDataset(X_test.to_numpy(), labels=y_test.to_numpy())
     return dataset
 
-
-
+# DRL iterator
 class RL_dataloader:
     def __init__(self, dataloader):
         self.loader = dataloader
@@ -165,9 +150,7 @@ class RL_dataloader:
 
         return data, label
 
-
-
-
+# DRL replay buffer
 class ReplayBuffer(object):
     def __init__(self):
         self.storage = []
@@ -207,34 +190,4 @@ class ReplayBuffer(object):
         return (np.array(s).squeeze(0), np.array(a1).squeeze(0), np.array(a2).squeeze(0),
                 np.array(n).squeeze(0), np.array(r).squeeze(0), np.array(d).squeeze(0).reshape(-1, 1), np.array(t).squeeze(0))
 
-
-
-MINORITY_CLASSES = [0, 1]
-def is_minority_class(generated_sample, classifier=None):
-    """
-    Checks if the generated sample belongs to a minority class.
-
-    Args:
-        generated_sample: The generated data sample.
-        classifier: (Optional) A pre-trained classifier to predict the sample's class if not explicitly available.
-
-    Returns:
-        bool: True if the sample belongs to a minority class, False otherwise.
-    """
-    # 1. Extract the class label directly, or use a classifier if the label is not provided
-    if classifier:  # Use a classifier to predict the class
-        _, sample_class = classifier.classify(generated_sample)
-    else:
-        raise ValueError("no classifier provided for prediction.")
-
-    # 2. Check if the class is in the minority classes
-    return sample_class in MINORITY_CLASSES
-
-
-def calculate_reward(classifier, reward, generated_sample, minority_class_ratio=3):
-    if is_minority_class(generated_sample, classifier):
-        minority_boost = 1 / minority_class_ratio
-        return reward * minority_boost
-
-    return reward
 
