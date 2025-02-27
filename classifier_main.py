e1f55b9339049e886a294b810ac14ca7bc4af775
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2025-02-27 10:38:36 +0100

    Update classifier_main.py

--- classifier_main.py
+++ classifier_main.py
@@ -11,6 +11,7 @@ from clfs import classifier
 def parse_args(args):
     parser = argparse.ArgumentParser()
 
+    # batch sizes and epochs
     parser.add_argument('--batch_size_train', default=32, type=int)
     parser.add_argument('--batch_size_test', default=64, type=int)
     parser.add_argument('--numEpochs', default=3, type=int)
@@ -22,14 +23,15 @@ def parse_args(args):
     # if test: ---train False
     parser.add_argument('--train', action='store_true')
     # unaug = unaugmented dataset = original dataset : if False then augmented dataset
-    parser.add_argument("--unaug_dataset", default=True)
-    parser.add_argument("--dataset_file", default="ds_1.csv")
+    parser.add_argument("--unaug_dataset", action = "store_true")
     # Generate labels for synthetic dataset
     parser.add_argument("--label_gen", action="store_true")
-    parser.add_argument("--synth_dataset", default=False)
-    parser.add_argument("--synth_dataset_path", default="/home/silver/PycharmProjects/AAEDRL/DDPG/rl_bal1.csv")
+    parser.add_argument("--synth_dataset", default=False) 
+    parser.add_argument("--synth_dataset_path", default="/home/silver/PycharmProjects/AAEDRL/DRL/rl_bal1.csv") # path to unsupervised dataset
     # PLEASE USE THE ABSOLUTE PATH IF YOU GET A "NO FILE IS FOUND" ERROR!!!
-    parser.add_argument("--labels_file", default = "labels1.csv")
+    # save labels
+    parser.add_argument("--labels_file", default = "labels.csv")
+    # save classifier state dictionary
     parser.add_argument("--save_state_dict", default="best_model.pth")
 
     return parser.parse_args(args)
@@ -37,12 +39,16 @@ def parse_args(args):
 if __name__ == "__main__":
     args = sys.argv[1:]
     args = parse_args(args)
+
+    print("using", args.unaug_dataset)
     original_ds = utils.dataset(original=args.unaug_dataset, train=args.train)
     train_loader, val_loader = utils.dataset_function(original_ds, args.batch_size_train, args.batch_size_test, train=True)
     test_loader = utils.dataset_function(original_ds, args.batch_size_train, args.batch_size_test, train=False)
     if args.train:
         for epoch in range(args.numEpochs):
+            # classifier pre-training
             train_loss = classifier.classifier_train(train_loader)
+            # evaluation
             if epoch % 10 == 0:
                 val_loss = classifier.classifier_val(val_loader)
                 best_val_loss = args.loss_threshold
@@ -51,10 +57,12 @@ if __name__ == "__main__":
                     torch.save(classifier.classifier.state_dict(), f'{args.save_state_dict}')
 
     else:
+        # testing
         classifier.classifier_test(args.save_state_dict, test_loader)
 
     if args.label_gen:
-        print("gen")
+        # generate labels
+        print("gen labels")
         labels, cl = classifier.gen_labels(args.save_state_dict, args.synth_dataset_path, args.batch_size_train)
         labels = labels.detach().cpu().numpy()
         labels = pd.DataFrame(labels, columns=["attack_cat"])
